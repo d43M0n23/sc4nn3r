1b213ca8cca8d6e09ef0ac0379c246191f18b169
daemon-r7 <daemon@coremedia-solution.com>
2017-09-12 21:34:04 +0200

    update

--- sc4nn3r.sh
+++ sc4nn3r.sh
@@ -31,35 +31,86 @@ blue='\033[34m'			#Schriftfarbe blau
 mag='\033[35m'			#Schriftfarbe magenta
 turk='\033[36m'			#Schriftfarbe türkis
 white='\033[37m'		#Schriftfarbe weiß
-## B4ckgr0unD
-bgblack='\033[40m'		#Hintergrund schwarz
-bgred='\033[41m'		#Hintergrund rot
-bggreen='\033[42m'		#Hintergrund grün
-bgyell='\033[43m'		#Hintergrund gelb
-bgblue='\033[44m'		#Hintergrund blau
-bgmag='\033[45m'		#Hintergrund magenta
-bgturk='\033[46m'		#Hintergrund türkis
-bgwhite='\033[47m'		#Hintergrund weiß
 #######################################################################
 
 ##Variablen
 DATE=$(date +%F)
+version=1.0
+vdate=11.09.2017
 
 # Local Destination
-DESTINATION=/home/bk4/
-
-# Database Backup User
-DATABASE_USER='root'
-DATABASE_PASSWORD=''
-DATABASE_HOST='localhost'
+#DESTINATION=/home/bk4/
 
 # Logfile
-LOG="bk4.log"
+#LOG="bk4.log"
 
 # Tools install
 #if ! hash exiftool 2>/dev/null; then sudo apt-get update && apt-get upgrade -y; sudo apt-get install --yes exiftool ; fi
 
 # Log & bka-dir check
-if [ ! -f $LOG ]; then touch $LOG; fi
-if [ ! -d $DESTINATION ]; then mkdir $DESTINATION; fi
+#if [ ! -f $LOG ]; then touch $LOG; fi
+#if [ ! -d $DESTINATION ]; then mkdir $DESTINATION; fi
 
+#wpscan --url http://bieradvent.bernyboi.com/ -enumerate
+
+#################
+clear
+#################
+echo '+-------------------------------------------------+'
+echo "|   Version ${version} (c)opyright 2017 by               |"
+echo '|   DaemoN d43M0n23@3xpl0it.com                   |'
+echo '+-------------------------------------------------+'
+echo '|   This Script is subject to the GPL License!    |'
+echo '|   You can copy and change it!                   |'
+echo '+-------------------------------------------------+'
+echo "| to continue press Return       Date: ${vdate} |"
+echo '+-------------------------------------------------+'
+read -e TASTE
+clear
+echo '+-------------------------------------------------+'
+echo '| Own-Scripts, Aux & Scanner-Frames               |'
+echo '+-------------------------------------------------+'
+echo '| 1.Arachni.                                      |'
+echo '| 2.Nessus.                                       |'
+echo '| 3.Getsploit.                                    |'
+echo '+-------------------------------------------------+'
+read -p "Attacker Nr (1-4)? " attacker
+##
+## Schleife
+##
+if [ $debupdate = y ]; then
+        apt-get update && apt-get upgrade -y
+        apt-get dist-upgrade -y
+        apt autoremove -y
+        apt-get clean
+        echo "thx 4 update"
+else
+        echo "
+install without update!
+"
+fi
+#exit
+
+
+#case "$1" in
+case "$attacker" in
+        1)
+		echo "Arachni selected"
+            ;;
+        2)
+		echo "Nessus selected"
+            ;;
+        3)
+		echo "Getsploit selected"
+            ;;
+        4)
+            if test "x`pidof anacron`" != x; then
+                stop
+                start
+            fi
+            ;;
+        *)
+
+	echo $"Usage: $0 {start|stop|restart|condrestart|status}"
+	exit 1
+esac
