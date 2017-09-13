e543aa29a54adf6a1369f28631a09c58e0efcff6
daemon-r7 <daemon@coremedia-solution.com>
2017-09-13 12:28:03 +0200

    update

--- sc4nn3r.sh
+++ sc4nn3r.sh
@@ -51,7 +51,13 @@ vdate=11.09.2017
 #if [ ! -f $LOG ]; then touch $LOG; fi
 #if [ ! -d $DESTINATION ]; then mkdir $DESTINATION; fi
 
+#HELPS
 #wpscan --url http://bieradvent.bernyboi.com/ -enumerate
+#do_something 2>&1 | tee -a some_file
+#&>filename (only log)
+#bash your_script.sh 1>file.log 2>&1
+
+TOPDIR=`pwd`
 
 #################
 clear
@@ -65,52 +71,69 @@ echo '|   You can copy and change it!                   |'
 echo '+-------------------------------------------------+'
 echo "| to continue press Return       Date: ${vdate} |"
 echo '+-------------------------------------------------+'
-read -e TASTE
-clear
+#read -e TASTE
+sleep 2
+while [ "$attacker" != "q" ]
+#clear
 echo '+-------------------------------------------------+'
 echo '| Own-Scripts, Aux & Scanner-Frames               |'
 echo '+-------------------------------------------------+'
-echo '| 1.Arachni.                                      |'
-echo '| 2.Nessus.                                       |'
-echo '| 3.Getsploit.                                    |'
+echo '| 1.Wpscan.                                       |'
+echo '| 2.CMSmap.                                       |'
+echo '| 3.D-TECT.                                       |'
+echo '| 4.WPSeku.                                       |'
+echo '| 5.Nikto.                                        |'
+echo '| 6.Reverse IP Lookup.                            |'
+echo '| x.Quit.                                         |'
 echo '+-------------------------------------------------+'
 read -p "Attacker Nr (1-4)? " attacker
-##
-## Schleife
-##
-if [ $debupdate = y ]; then
-        apt-get update && apt-get upgrade -y
-        apt-get dist-upgrade -y
-        apt autoremove -y
-        apt-get clean
-        echo "thx 4 update"
-else
-        echo "
-install without update!
-"
-fi
-#exit
 
 
-#case "$1" in
-case "$attacker" in
+#PS3="Attacker Nr. - Ihre Wahl : "
+#attacker=("1" "2" "D-TECT" "quit")
+#select auswahl in Punkt1 Punkt2 Punkt3 Punkt4 Ende
+#select auswahl in "${attacker[@]}"
+	do
+case $attacker in
+#case $auswahl in
         1)
-		echo "Arachni selected"
-            ;;
+		echo "Wpscan selected"
+		read -p "domain (e.g. google.com)? " wp_domain
+		if [ $wp_domain ]; then
+		wpscan --url $wp_domain --enumerate 2>&1 | tee -a wpscan.log
+		echo -e "${yell}Logfile is saved as wpscan.log${clear}"
+		else
+        echo "
+Please enter a domain!
+"
+	fi
+            	;;
         2)
-		echo "Nessus selected"
-            ;;
+		echo "CMSmap selected"
+		read -p "domain (e.g. google.com)? " cms_domain
+		python /root/c0r3/09-cms/CMSmap/cmsmap.py -t $cms_domain -o cmsscan.log
+            	;;
         3)
-		echo "Getsploit selected"
-            ;;
+		echo "D-TECT selected"
+		python /root/c0r3/09-cms/D-TECT/d-tect.py
+		;;
         4)
-            if test "x`pidof anacron`" != x; then
-                stop
-                start
-            fi
-            ;;
+		echo "WPSeku selected"
+		read -p "domain (e.g. google.com)? " wpseku_domain
+		python /root/c0r3/09-cms/WPSeku/wpseku.py -t $wpseku_domain 2>&1 | tee -a wpseku.log
+            	;;
+        5)
+                echo "Nikto selected"
+                read -p "domain (e.g. google.com)? " nikto_domain
+                nikto -host http://$nikto_domain 2>&1 | tee -a nikto.log
+                ;;
+
+	x)
+		break
+		;;
         *)
 
-	echo $"Usage: $0 {start|stop|restart|condrestart|status}"
-	exit 1
-esac
+		echo $"Usage: $0 {start|stop|restart|condrestart|status}"
+		exit 1
+	esac
+done
